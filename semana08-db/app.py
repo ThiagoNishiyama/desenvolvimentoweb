039edcf1944d6ca55397496b479672d934a8abc9
ThiagoNY <thiagonishiyama10@outlook.com>
2026-09-06 19:12:15 -0300

    Semana 08 - Bancos de dados

---
+++ semana08-db/app.py
@@ -0,0 +1,74 @@
+import os
+from flask import Flask, render_template, session, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'chave-secreta-semana08'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+with app.app_context():
+    db.create_all()
+    if not Role.query.filter_by(name='Administrator').first():
+        db.session.add(Role(name='Administrator'))
+    if not Role.query.filter_by(name='User').first():
+        db.session.add(Role(name='User'))
+    db.session.commit()
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user_role = Role.query.filter_by(name='User').first()
+            user = User(username=form.name.data, role=user_role)
+            db.session.add(user)
+            db.session.commit()
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    users = User.query.join(Role).order_by(User.id).all()
+    return render_template(
+        'index.html',
+        form=form,
+        name=session.get('name'),
+        users=users,
+    )
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
